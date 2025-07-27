d082201483b63417b0b245dfc2eeea134544a70e
SarangRao20 <raosarang2006@gmail.com>
2025-07-27 09:49:49 +0530

    Testing Groq api with Tesseract-OCR

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 print("HI")
 name = input("ENter your name: ")
 print(name)
+
+flask = __import__('flask')
+app = flask.Flask(__name__)
+@app.route('/')
+def hello_world():
+    return f'Hello, {name}!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
